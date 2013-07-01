15dc314854b535d742fa7e42337a36c7fd19dbd8
Alexandrine Boissière <theasta@gmail.com>
2013-07-01 12:04:00 -0700

    Added an external .jshintrc file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,7 @@
-/*
- * grunt-cli
- * http://gruntjs.com/
- *
- * Copyright (c) 2012 Tyler Kellen, contributors
- * Licensed under the MIT license.
- * https://github.com/gruntjs/grunt-init/blob/master/LICENSE-MIT
- */
-
 'use strict';
 
 module.exports = function(grunt) {
 
-  // Project configuration.
   grunt.initConfig({
     nodeunit: {
       tests: [ 'test/*_test.js' ]
@@ -28,18 +18,7 @@ module.exports = function(grunt) {
         'bin/*',
       ],
       options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        node: true
+        jshintrc: '.jshintrc'
       }
     },
     watch: {
@@ -50,15 +29,11 @@ module.exports = function(grunt) {
     }
   });
 
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // "npm test" runs these tasks
   grunt.registerTask('test', ['jshint', 'nodeunit']);
-
-  // Default task.
   grunt.registerTask('default', ['test']);
 
 };
